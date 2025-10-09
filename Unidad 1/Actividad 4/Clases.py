37d8cfef1f88e8273898cfee203a5f922465d52f
Joham <alguien_bama@hotmail.com>
2025-10-08 19:32:55 -0500

    Act 4: Creacion de la clase Libro

---
+++ Unidad 1/Actividad 4/Clases.py
@@ -0,0 +1,20 @@
+class Libro:
+    def __init__(self,titulo,autor,publicacon,prestado=False):
+        self.titulo = titulo
+        self.autor = autor
+        self.publicacon = publicacon
+        self.prestado = prestado
+    bibliteca = "Biblbioteca Central"
+    def prestar():
+        if self.prestado == False:
+            self.prestado = True
+            return "El libro ha sido prestado"
+        else:
+            return "El libro ya esta prestado"
+    def devolver():
+        if self.prestado == True:
+            self.prestado = False
+            return "El libro ha sido devuelto"
+        else:
+            return "El libro no estaba prestado"
+    mostrar_estado = self.titulo + " de " + self.autor + " publicado en " + str(self.publicacon) + ".\nEstado: " + ("Prestado" if self.prestado else "Disponible")
